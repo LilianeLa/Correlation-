2a5a50c36338c6245d552fa9e584d2d7f4719f65
LilianeLa <LilianeLa@users.noreply.github.com>
2015-04-17 16:04:43 +0200

    Detection of BP and P300 for SA14 and SA34

---
+++ Detected BP & P300: results.m
@@ -0,0 +1,229 @@
+>> find_P300(SA14_11_07_2013_0003_Chs2_c676b9f94033a51d1230__Ch11.times, SW_SA14_11_07_2013_0003)
+ 
+OA1 occurs at 26.1437s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA2 occurs at 61.4027s
+     -> No BP detected
+     -> Possible P3:    negative peak at 62.975s;    positive peak at 63.1853s
+ 
+OA3 occurs at 91.1019s
+     -> Possible BP:    negative peak at 90.0769s;    positive peak at 90.3833s
+     -> Possible P3:    negative peak at 92.8844s;    positive peak at 93.2181s
+                        negative peak at 93.472s;    positive peak at 93.6588s
+                        negative peak at 94.2332s;    positive peak at 94.4167s
+ 
+For a total of 3 operant actions,
+ - 1 operant action shows at least one BP.
+ - 2 operant actions show at least one P3.
+>> 
+
+
+
+>> find_P300(SA14_11_07_2013_0004_Chs2_4a6570a17115fe26f535__Ch11.times, SW_SA14_11_07_2013_0004)
+ 
+OA1 occurs at 26.103s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA2 occurs at 71.1319s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA3 occurs at 101.1506s
+     -> No BP detected
+     -> No P3 detected
+ 
+For a total of 3 operant actions,
+ - No operant action shows at least one BP.
+ - No operant action shows at least one P3.
+>> 
+
+
+>> find_P300(SA34_20_06_2014_0003_Ch11.times, SW_SA34_20_06_2014_0003)
+ 
+OA1 occurs at 26.0841s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA2 occurs at 342.7961s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA3 occurs at 2073.5323s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA4 occurs at 2279.535s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA5 occurs at 3615.2404s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA6 occurs at 3616.2004s
+     -> No BP detected
+     -> No P3 detected
+ 
+For a total of 6 operant actions,
+ - No operant action shows at least one BP.
+ - No operant action shows at least one P3.
+>> 
+
+
+>> find_P300(SA34_27_06_2014_0003_Ch11.times, SW_SA34_27_06_2014_0003)
+ 
+OA1 occurs at 73.0474s
+     -> Possible BP:    negative peak at 70.6696s;    positive peak at 71.2095s
+     -> No P3 detected
+ 
+OA2 occurs at 94.4068s
+     -> Possible BP:    negative peak at 94.3924s;    positive peak at 94.9758s
+     -> No P3 detected
+ 
+OA3 occurs at 137.0757s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA4 occurs at 454.704s
+     -> Possible BP:    negative peak at 453.755s;    positive peak at 454.6112s
+     -> No P3 detected
+ 
+OA5 occurs at 646.191s
+     -> Possible BP:    negative peak at 643.566s;    positive peak at 644.2603s
+     -> No P3 detected
+ 
+OA6 occurs at 740.7517s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA7 occurs at 786.7606s
+     -> Possible BP:    negative peak at 786.3668s;    positive peak at 786.688s
+     -> No P3 detected
+ 
+OA8 occurs at 821.5397s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA9 occurs at 846.777s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA10 occurs at 1028.9726s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA11 occurs at 1063.2517s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA12 occurs at 1313.9755s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA13 occurs at 1425.6426s
+     -> Possible BP:    negative peak at 1424.7573s;    positive peak at 1425.4816s
+     -> No P3 detected
+ 
+OA14 occurs at 1526.5001s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA15 occurs at 1695.1161s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA16 occurs at 1709.6157s
+     -> No BP detected
+     -> No P3 detected
+ 
+For a total of 16 operant actions,
+ - 6 operant actions show at least one BP.
+ - No operant action shows at least one P3.
+>> 
+
+
+>> find_P300(SA34_04_07_2014_0001_Ch11.times, SW_SA34_04_07_2014_0001)
+ 
+OA1 occurs at 11.9322s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA2 occurs at 91.2904s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA3 occurs at 231.6503s
+     -> Possible BP:    negative peak at 226.1818s;    positive peak at 227.063s
+     -> No P3 detected
+ 
+OA4 occurs at 729.7041s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA5 occurs at 1048.0822s
+     -> Possible BP:    negative peak at 1047.9054s;    positive peak at 1048.3495s
+     -> No P3 detected
+ 
+OA6 occurs at 1134.7987s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA7 occurs at 1135.2787s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA8 occurs at 1136.7187s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA9 occurs at 1355.8934s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA10 occurs at 1526.9283s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA11 occurs at 1899.4379s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA12 occurs at 2149.7741s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA13 occurs at 2157.6708s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA14 occurs at 2394.422s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA15 occurs at 2412.5001s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA16 occurs at 2925.2096s
+     -> Possible BP:    negative peak at 2924.5049s;    positive peak at 2925.4961s
+     -> No P3 detected
+ 
+OA17 occurs at 3201.6715s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA18 occurs at 3497.902s
+     -> No BP detected
+     -> No P3 detected
+ 
+OA19 occurs at 3984.7955s
+     -> No BP detected
+     -> No P3 detected
+ 
+For a total of 19 operant actions,
+ - 3 operant actions show at least one BP.
+ - No operant actions show at least one P3.
+>> 
