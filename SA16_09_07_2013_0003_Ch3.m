5ca57b450b137e7b822d7ac848c85e8587848721
LilianeLa <LilianeLa@users.noreply.github.com>
2015-04-23 16:00:38 +0200

    SA16 day 6 (part 2)

---
+++ SA16_09_07_2013_0003_Ch3.m
@@ -0,0 +1,86 @@
+>> principal(SA16_09_07_2013_0003_Ch3.values, ttl.times);
+ 
+*********************************************************************************************************
+                                       I. DETECTION OF ARTIFACTS                                         
+*********************************************************************************************************
+ 
+A1 begins at 150.7911s;    150.7925s: end of fast comp.;            (there's no slow comp.)
+                                                           150.7925s: end of total slow comp.
+                                                           Nothing above diapason from 150.7925s to 151.4425s
+ 
+A2 begins at 158.2904s;    158.2904s: end of fast comp.;            (there's no slow comp.)
+                                                           158.2904s: end of total slow comp.
+                                                           Nothing above diapason from 158.2904s to 158.9404s
+ 
+A3 begins at 174.7369s;    174.737s: end of fast comp.;            (there's no slow comp.)
+                                                           174.737s: end of total slow comp.
+                                                           Nothing above diapason from 174.737s to 175.387s
+ 
+A4 begins at 186.4664s;    186.4664s: end of fast comp.;            (there's no slow comp.)
+                                                           186.4664s: end of total slow comp.
+                                                           Nothing above diapason from 186.4664s to 187.1164s
+ 
+A5 begins at 262.4547s;    262.4547s: end of fast comp.;            (there's no slow comp.)
+                                                           263.3566s: end of total slow comp.
+                                                           Sth above diapason (262.4547s to 263.1047s)
+ 
+A6 begins at 396.5218s;    396.5218s: end of fast comp.;            (there's no slow comp.)
+                                                           396.6277s: end of total slow comp.
+                                                           Sth above diapason (396.5218s to 397.1719s)
+ 
+A7 begins at 405.5381s;    405.5381s: end of fast comp.;            (there's no slow comp.)
+                                                        405.5381s: end of total slow comp.
+                                                        Nothing above diapason from 405.5381s to 406.188s
+ 
+ 
+*********************************************************************************************************
+                                      II. DETECTION OF SLOW WAVES                                        
+*********************************************************************************************************
+ 
+ 
+SW1:    -67.4672 microV at 76.5457s;    4.1598 microV at 77.084s
+ 
+SW2:    -26.5639 microV at 92.6243s;    25.1976 microV at 93.4591s
+ 
+SW3:    -34.4029 microV at 96.2401s;    13.0152 microV at 97.1057s
+ 
+SW4:    -21.2604 microV at 107.7232s;    20.3947 microV at 108.8438s
+ 
+SW5:    -21.1972 microV at 219.4766s;    20.5045 microV at 220.1454s
+ 
+SW6:    -39.176 microV at 222.3464s;    24.1343 microV at 223.0202s
+ 
+SW7:    -28.1986 microV at 227.0151s;    18.0394 microV at 227.6793s
+ 
+SW8:    -81.4533 microV at 352.849s;    100.9783 microV at 354.0059s
+ 
+SW9:    -70.8809 microV at 354.5781s;    39.7384 microV at 355.2668s
+ 
+SW10:    -28.7408 microV at 414.4322s;    22.2898 microV at 414.9536s
+ 
+SW11:    -32.2947 microV at 426.9006s;    23.8598 microV at 427.7444s
+ 
+----> 11 slow waves detected
+ 
+ 
+*********************************************************************************************************
+              III. DETECTION OF SLOW WAVES OCCURRING JUST BEFORE AND AFTER OPERANT ACTIONS               
+*********************************************************************************************************
+ 
+OA1 occurs at 350.0218s
+     -> No BP detected
+     -> Possible P3:    -81.4533 microV at 352.849s;    100.9783 microV at 354.0059s
+                        -70.8809 microV at 354.5781s;    39.7384 microV at 355.2668s
+ 
+ 
+Random window 1: [359s, 369s]
+     -> No SW detected
+ 
+*********************************************************************************************************
+There is only 1 operant action.
+ - No operant action shows at least one BP.
+ - 1 operant action shows at least one P3.
+ - 0 of the 1 random windows shows at least one SW.
+*********************************************************************************************************
+ 
+>> 
